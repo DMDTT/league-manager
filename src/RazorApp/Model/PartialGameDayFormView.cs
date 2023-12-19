e12b55593a108ed0e8e6e258d60fedce4dd198cd
DMDTT <a.tischer86@gmail.com>
2023-12-19 12:16:19 +0100

    move gameday forms to partial view

---
+++ src/RazorApp/Model/PartialGameDayFormView.cs
@@ -0,0 +1,10 @@
+﻿namespace RazorApp.Model;
+
+public class PartialGameDayFormView
+{
+    public int TeamId { get; set; }
+    public int LeagueId { get; set; }
+    public int? SelectedGameDay { get; set; }
+    public int MatchId { get; set; }
+    public int Goals { get; set; }
+}
